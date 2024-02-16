0522485e67526d0749af4f24f560e798ad093670
Giacomo Borin <d.borin68.gb@gmail.com>
2024-02-16 21:03:10 +0100

    crypto action

--- action.py
+++ action.py
@@ -6,11 +6,24 @@ from sage.all import randint, ZZ, factor, proof
 from sage.categories.action import Action
 
 
+
 class CryptoAction(Action):
-    def __init__(self):
-        pass 
+    def __init__(self,G,S,security):
+        super().__init__(G,S)
+        self.security = security
 
-    pass
+    def rand_set(self, SEED = None):
+        with seed(SEED): return (self.domain()).random_element()
 
+    def rand_group(self, SEED = None):
+        with seed(SEED): return (self.actor()).random_element()
 
+    def origin(self):
+        return self.rand_set(SEED = 1)
+
+    def set_costs(self):
+        pass
+
+    def group_costs(self):
+        pass
 
